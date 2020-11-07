3f4c656e50d4560c8bd9966c0555dd6891ded797
Bruno Gaspar <bruninhoogaspar@gmail.com>
2020-11-07 17:23:57 -0300

    Added listFlights(), showFlight() and listAirlineInfo()

--- src/br/com/ifpr/codes/Main.java
+++ src/br/com/ifpr/codes/Main.java
@@ -1,5 +1,7 @@
 package br.com.ifpr.codes;
 
+import java.sql.Date;
+import java.text.SimpleDateFormat;
 import java.util.Scanner;
 
 public class Main {
@@ -13,7 +15,7 @@ public class Main {
 
     company.scanner = new Scanner(System.in);
 
-    System.out.println("\n----- Companhia Aérea -----\n");
+    System.out.println("\n------ Companhia Aérea ------\n");
 
     System.out.print("Informe o nome: ");
     company.airline.setName(company.scanner.nextLine());
@@ -31,13 +33,13 @@ public class Main {
     String option = "";
 
     while (!option.equals("5")) {
-      System.out.println("\n---------------------------");
+      System.out.println("\n------------------------------");
       System.out.println("[1] Cadastrar novo vôo");
       System.out.println("[2] Listar vôos existentes");
       System.out.println("[3] Consultar vôo");
-      System.out.println("[4] Listar companhias");
+      System.out.println("[4] Listar dados da companhia");
       System.out.println("[5] Sair");
-      System.out.println("---------------------------");
+      System.out.println("------------------------------");
       System.out.print("--> ");
 
       option = this.scanner.nextLine();
@@ -50,10 +52,10 @@ public class Main {
           listFlights();
           break;
         case "3":
-          // showFlight();
+          showFlight();
           break;
         case "4":
-          // listAirlines();
+          listAirlineInfo();
           break;
         case "5":
           System.out.println("\nEncerrando...\n");
@@ -68,10 +70,12 @@ public class Main {
   public void newFlight() {
     try {
       Flight flight = new Flight();
+      SimpleDateFormat formatter = new SimpleDateFormat("dd-MM-yyyy");
+      Date date = new Date(System.currentTimeMillis());
 
       Boolean isInternational = true;
 
-      System.out.println("\n------- Novo Vôo -------\n");
+      System.out.println("\n--------- Novo Vôo ---------\n");
 
       System.out.print("Informe o número do vôo: ");
       flight.setFlightNumber(this.scanner.nextLine());
@@ -86,14 +90,12 @@ public class Main {
       flight.setDestination(this.scanner.nextLine());
 
       System.out.print("Vôo internacional? [S/N] ");
-      String option = this.scanner.nextLine();
+      String option = this.scanner.nextLine().toUpperCase();
       switch (option) {
         case "S":
-        case "s":
           flight.setIsInternational(isInternational);
           break;
         case "N":
-        case "n":
           flight.setIsInternational(!isInternational);
           break;
         default:
@@ -102,7 +104,9 @@ public class Main {
           break;
       }
 
-      System.out.println("\n------- Passageiros -------");
+      flight.setDate(formatter.format(date).toString());
+
+      System.out.println("\n--------- Passageiros ---------");
 
       for (int i = 0; i < 10; i++) {
         Passenger passenger = new Passenger();
@@ -138,12 +142,106 @@ public class Main {
   }
 
   public void listFlights() {
-    Flight flight = new Flight();
+    System.out.println("------------------------------");
+    System.out.println("Relatorio de Vôo");
 
-    System.out.println(flight.getAirplaneNumber());
-    System.out.println(flight.getVetPassenger(0));
+    System.out.println("\nCompanhia: " + this.airline.getName());
 
-    System.out.println(this.airline.getVetFlights(0));
+    for (int i = 0; i < this.airline.getFlightsCount(); i++) {
+      Flight flightInfo = this.airline.getVetFlights(i);
+
+      String flightNumber = "Vôo: " + flightInfo.getFlightNumber();
+
+      String airplaneNumber = "Avião: " + flightInfo.getAirplaneNumber();
+
+      String flightOrigin = "Local de origem: " + flightInfo.getOrigin();
+
+      String flightDestination = "Local de destino: " + flightInfo.getDestination();
+
+      String flightIsInternational = flightInfo.getIsInternational() ? "Internacional: [X]" : "Internacional: []";
+
+      String flightDate = "Data do vôo: " + flightInfo.getDate();
+
+      String finalFlightInfo = "\n" + flightNumber + " | " + airplaneNumber + " | " + flightOrigin + "\n"
+          + flightDestination + " | " + flightIsInternational + " | " + flightDate;
+      System.out.println(finalFlightInfo);
+    }
+
+    System.out.print("\nPressione enter para continuar...");
+    this.scanner.nextLine();
+  }
+
+  public void showFlight() {
+    try {
+      System.out.println("------------------------------");
+      System.out.println("Consulta de Vôo");
+
+      System.out.print("Informe o número do Vôo: ");
+      String flightNumber = this.scanner.nextLine();
+
+      Boolean findFlight = false;
+
+      for (int i = 0; i < this.airline.getFlightsCount(); i++) {
+        Flight flightInfo = this.airline.getVetFlights(i);
+
+        if (flightInfo.getFlightNumber().equals(flightNumber)) {
+          System.out.println("\nPassageiros do Vôo: ");
+
+          int index = 0;
+
+          System.out.println("\nVôo: " + flightNumber);
+
+          while (flightInfo.getVetPassenger(index) != null) {
+            Passenger passengerInfo = flightInfo.getVetPassenger(index);
+
+            String passengerName = "Nome: " + passengerInfo.getName();
+
+            String passengerDoc = "Documento: " + passengerInfo.getDocNumber();
+
+            String passengerNacionality = "Nacionalidade: " + passengerInfo.getNacionality();
+
+            String passengerAge = "Idade: " + passengerInfo.getAge();
+
+            String finalPassengerInfo = "\n" + passengerName + " | " + passengerDoc + " | " + passengerNacionality
+                + " | " + passengerAge;
+            System.out.println(finalPassengerInfo);
+
+            index++;
+          }
+
+          findFlight = true;
+          break;
+        }
+      }
+
+      if (!findFlight) {
+        System.out.println("Vôo não encontrado!");
+      }
+
+      System.out.print("\nPressione enter para continuar...");
+      this.scanner.nextLine();
+    } catch (Exception error) {
+      System.out.println("Opa! Algo de errado não está certo.");
+    }
+  }
+
+  public void listAirlineInfo() {
+    System.out.println("------------------------------");
+    System.out.println("Informações da Companhia");
+
+    Airline airlineInfo = this.airline;
+
+    String airlineName = "\nNome: " + airlineInfo.getName();
+
+    String airlineTelephone = "Telefone: " + airlineInfo.getTelephone();
+
+    String airlineEmail = "E-mail: " + airlineInfo.getEmail();
+
+    String finalAirlineInfo = airlineName + " | " + airlineTelephone + " | " + airlineEmail;
+    System.out.println(finalAirlineInfo);
+
+    System.out.print("\nPressione enter para continuar...");
+    this.scanner.nextLine();
   }
 
   public void clearBuffer(Scanner scanner) {
