89bfe255ef13b66c974bd15de65150949c26dc40
Bruno Gaspar <bruninhoogaspar@gmail.com>
2021-11-23 14:51:47 -0300

    fix: add cors

--- src/app.ts
+++ src/app.ts
@@ -15,7 +15,8 @@ const app = express()
 
 app.use(
   cors({
-    origin: "*",
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    origin: "https://mark-one-frontend.vercel.app",
   })
 )
 app.use(express.json())
