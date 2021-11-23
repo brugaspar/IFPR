d0cb2e8fb62ac61e528c44805f0e85a9df118865
Bruno Gaspar <bruninhoogaspar@gmail.com>
2021-11-23 15:00:53 -0300

    fix: removed timeout

--- src/services/api.service.ts
+++ src/services/api.service.ts
@@ -1,9 +1,9 @@
 import axios from "axios"
 
-const FIVE_SECONDS = 5000
+// const FIVE_SECONDS = 5000
 
 export const api = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL,
-  timeout: FIVE_SECONDS,
-  timeoutErrorMessage: "Tempo da requisição esgotou",
+  // timeout: FIVE_SECONDS,
+  // timeoutErrorMessage: "Tempo da requisição esgotou",
 })
