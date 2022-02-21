f25823af33cebbd3aea88c1d5f541c7c7af52f8e
9818lucas <lucaszorzan14@gmail.com>
2022-02-20 21:36:09 -0300

    criação do dashboard administrativo

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,11 +1,68 @@
 import { Header } from "../../components/Header";
 
-import { Container } from "./styles";
+import { 
+  Container,
+  DashCard_fullRow,
+  DashCard_fullRowContainer,
+  DashCard_halfRow,
+  DashCard_row,
+  DashCardTitle,
+  DashCardValue,
+  Highlight,
+  Message, 
+} from "./styles";
 
 export function Dashboard() {
   return (
     <Container>
       <Header />
+      <Message>
+      Esse é um resumo dos{"\n"}totais do <Highlight>clube</Highlight>
+      </Message>
+      <DashCard_fullRowContainer>
+        <DashCardTitle>Valor total das{"\n"}atividades</DashCardTitle>
+        <DashCardValue>R$ 2.332,81</DashCardValue>
+      </DashCard_fullRowContainer>
+
+      <DashCard_fullRowContainer>
+        <DashCardTitle>Atividades{"\n"}cadastradas</DashCardTitle>
+        <DashCardValue>19</DashCardValue>
+      </DashCard_fullRowContainer>
+
+      <DashCard_row>
+        <DashCard_halfRow>
+          <DashCardTitle>Membros ativos</DashCardTitle>
+          <DashCardValue>362</DashCardValue>
+        </DashCard_halfRow>
+
+        <DashCard_halfRow>
+          <DashCardTitle>Usuários ativos</DashCardTitle>
+          <DashCardValue>19</DashCardValue>
+        </DashCard_halfRow>
+      </DashCard_row>
+
+      <DashCard_row>
+          <DashCard_halfRow>
+            <DashCardTitle>Planos Ativos</DashCardTitle>
+            <DashCardValue>3</DashCardValue>
+          </DashCard_halfRow>
+
+          <DashCard_halfRow>
+            <DashCardTitle>Produtos ativos</DashCardTitle>
+            <DashCardValue>243</DashCardValue>
+          </DashCard_halfRow>
+      </DashCard_row>
+
+      <DashCard_fullRowContainer>
+        <DashCardTitle>Ticket médio</DashCardTitle>
+        <DashCardValue>R$ 14,67</DashCardValue>
+      </DashCard_fullRowContainer>
+
+      <DashCard_fullRowContainer>
+        <DashCardTitle>Atividades{"\n"}em aberto</DashCardTitle>
+        <DashCardValue>9</DashCardValue>
+      </DashCard_fullRowContainer>
+
     </Container>
   );
 }
--- src/screens/Dashboard/styles.ts
+++ src/screens/Dashboard/styles.ts
@@ -7,3 +7,70 @@ export const Container = styled.View`
   padding: 16px;
   background: ${styles.colors.background};
 `;
+
+export const Message = styled.Text`
+  font-family: ${styles.fonts.epilogueMedium};
+  font-size: 16px;
+  color: ${styles.colors.text};
+
+  line-height: 22px;
+  text-align: left;
+  margin-bottom:40px;
+
+  /* margin: 36px 0; */
+`;
+
+export const Highlight = styled.Text`
+  color: ${styles.colors.green};
+`;
+
+
+export const DashCard_fullRowContainer = styled.View`
+  background: ${styles.colors.input};
+  border-radius: 4px;
+  margin-bottom: 15px;
+  padding: 10px;
+  height:62px;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+
+export const DashCard_fullRow = styled.View`
+  background: ${styles.colors.input};
+  border-radius: 4px;
+  margin-bottom: 15px;
+  padding: 10px;
+  height:62px;
+`;
+
+export const DashCard_row = styled.View`
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+export const DashCardTitle = styled.Text`
+  font-family: ${styles.fonts.epilogueMedium};
+  font-size: 16px;
+  color: ${styles.colors.text};
+`;
+
+export const DashCardValue = styled.Text`
+  font-family: ${styles.fonts.nunitoBold};
+  font-size: 18px;
+  color: ${styles.colors.green};
+`;
+
+export const DashCard_halfRow = styled.View`
+  background: ${styles.colors.input};
+  border-radius: 4px;
+  margin-bottom: 15px;
+  padding: 10px;
+  height:82px;
+  width:48%;  
+`;
+
+
+
