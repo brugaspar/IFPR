d71e4a98d7e2aa7c6730e4223b1c8f63164ee685
Eduardo <contatoeduardorezes@gmail.com>
2022-03-09 21:19:23 -0300

    Adicionado Logo ao Sidebar

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,11 +1,14 @@
 import { Link } from "react-router-dom";
 
+import logoIfpr from "../../assets/img/IFPR Banner.png";
+
+
 import { Container } from "./styles";
 
 export function Sidebar() {
   return (
     <Container>
-      <div>IFPR LOGO</div>
+      <img src={logoIfpr}/>
       <div>
         <Link to="/">Provas</Link>
         <Link to="questions">Questões</Link>
