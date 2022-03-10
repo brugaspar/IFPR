8d90895b7dea18462fbc67f7557667e023e2c270
Eduardo <contatoeduardorezes@gmail.com>
2022-03-09 21:19:59 -0300

    Adicionado stile para img

--- src/components/Sidebar/styles.ts
+++ src/components/Sidebar/styles.ts
@@ -11,6 +11,11 @@ export const Container = styled.div`
   background: var(--primary);
   color: var(--background);
 
+  img {
+    width:100%;
+    height:150px;
+  }
+
   display: flex;
   flex-direction: column;
   align-items: center;
