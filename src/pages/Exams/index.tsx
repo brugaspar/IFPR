6420876fd1ea892254e67e05e4f21c56772fa3e5
Bruno Gaspar <bruninhoogaspar@gmail.com>
2022-03-11 15:50:09 -0300

    fix: changed some texts

--- src/pages/Exams/index.tsx
+++ src/pages/Exams/index.tsx
@@ -2,44 +2,42 @@ import { Button } from "../../components/Button";
 import { Separator } from "../../components/Separator";
 import { Container } from "./styles";
 
-
 export function Exams() {
-  
   const challenges = [
     {
       id: "1",
       status: "Rascunho",
       title: "Prova IFPR - Estrutura de Dados 2022",
-      description: "Prova de conheicimento em Arvores.",
+      description: "Prova de conhecimentos em Árvores Binárias.",
       grade: "",
-      created_at: "09/03/2022",
-      finished_at: "",
+      createdAt: "09/03/2022",
+      finishedAt: "",
     },
     {
       id: "2",
       status: "Criada",
       title: "Prova IFPR - Padrões de Projetos e Framework 2022",
-      description: "Prova de conheicimento em Arvores.",
+      description: "Prova de conhecimentos em Padrões de Projetos.",
       grade: "",
-      created_at: "09/03/2022",
-      finished_at: "",
+      createdAt: "09/03/2022",
+      finishedAt: "",
     },
     {
       id: "3",
       status: "Encerrada",
-      title: "Prova IFPR - Desenvolvimento WebIV 2022",
-      description: "Prova de conheicimento em API.",
-      grade: "7,5",
-      created_at: "20/01/2022",
-      finished_at: "13/02/2022",
-    }
+      title: "Prova IFPR - Desenvolvimento Web IV 2022",
+      description: "Prova de conhecimentos em API.",
+      grade: "7.5",
+      createdAt: "20/01/2022",
+      finishedAt: "13/02/2022",
+    },
   ];
 
   return (
     <Container>
       <div className="flex-div">
         <h1>Lista de provas</h1>
-        <Button >Nova prova</Button>
+        <Button>Nova prova</Button>
       </div>
       <Separator />
       <table>
@@ -58,8 +56,8 @@ export function Exams() {
               <td title={challenge.title}>{challenge.title}</td>
               <td title={challenge.description}>{challenge.description}</td>
               <td title={challenge.grade}>{challenge.grade}</td>
-              <td title={challenge.created_at}>{challenge.created_at}</td>
-              <td title={challenge.finished_at}>{challenge.finished_at}</td>
+              <td title={challenge.createdAt}>{challenge.createdAt}</td>
+              <td title={challenge.finishedAt}>{challenge.finishedAt}</td>
             </tr>
           ))}
         </tbody>
