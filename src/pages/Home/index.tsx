69717b7b397d5a3fc50e5368e947c89f223151dc
Bruno Gaspar <bruninhoogaspar@gmail.com>
2022-03-22 20:02:01 -0300

    finished

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,3 +1,19 @@
+import { useNavigate } from "react-router-dom";
+import { Container } from "./styles";
+
 export function Home() {
-  return <h1>HOME</h1>;
+  const navigate = useNavigate();
+
+  function logout() {
+    navigate("/");
+  }
+
+  return (
+    <Container>
+      <h1>Olá, Usuário!</h1>
+      <button type="button" onClick={logout}>
+        Sair
+      </button>
+    </Container>
+  );
 }
